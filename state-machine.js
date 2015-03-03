5b1fd2b1ae14332231620bac3af1000da2973181
Mathieu Amiot <m.amiot@otak-arts.com>
2015-03-03 18:12:02 +0100

    Added key prefixing to redis commands + prepared redis config

--- state-machine.js
+++ state-machine.js
@@ -1,6 +1,12 @@
 var moves = require('./move-types.js'),
     Q = require('q');
 
+var prefix = 'slackpkmn:';
+
+var redisConfig = {
+    
+};
+
 var redis,
     rtg;
 
@@ -15,7 +21,7 @@ if(process.env.REDISTOGO_URL) {
   redis.auth(rtg.auth.split(":")[1]);
 } else {
   //then we're running locally
-  redis = require("redis").createClient();
+  redis = require("redis").createClient(redisConfig);
 }
 
 /* Turn Redis Methods Into Promise-returning Ones */
@@ -35,10 +41,10 @@ QRedis.smembers = Q.nbind(redis.smembers, redis);
 module.exports = {};
 
 module.exports.newBattle = function(playerName, channel) {
-  return QRedis.exists("currentBattle")
+  return QRedis.exists(prefix + "currentBattle")
     .then(function(exists){
       if(!exists) {
-        return QRedis.hmset("currentBattle", {
+        return QRedis.hmset(prefix + "currentBattle", {
           "playerName": playerName,
           "channel": channel
         })
@@ -49,25 +55,25 @@ module.exports.newBattle = function(playerName, channel) {
 }
 
 module.exports.getBattle = function() {
-  return QRedis.hgetall("currentBattle");
+  return QRedis.hgetall(prefix + "currentBattle");
 }
 
 module.exports.endBattle = function() {
   return QRedis.del([
-    "currentBattle",
-    "user:allowedMoves",
-    "npc:allowedMoves",
-    "npc:hp",
-    "user:hp",
-    "user:pkmnTypes",
-    "npc:pkmnTypes"
+    prefix + "currentBattle",
+    prefix + "user:allowedMoves",
+    prefix + "npc:allowedMoves",
+    prefix + "npc:hp",
+    prefix + "user:hp",
+    prefix + "user:pkmnTypes",
+    prefix + "npc:pkmnTypes"
   ])
 }
 
 module.exports.addMove = function(data) {
-  return QRedis.sadd("user:allowedMoves", data.name.toLowerCase())
+  return QRedis.sadd(prefix + "user:allowedMoves", data.name.toLowerCase())
     .then(function(addReturned){
-      return QRedis.hmset("move:"+data.name.toLowerCase(),{
+      return QRedis.hmset(prefix + "move:"+data.name.toLowerCase(),{
         "power": data.power,
         "type": moves.getMoveType(data.name.toLowerCase())
       })
@@ -75,9 +81,9 @@ module.exports.addMove = function(data) {
 }
 
 module.exports.addMoveNPC = function(data) {
-  return QRedis.sadd("npc:allowedMoves", data.name.toLowerCase())
+  return QRedis.sadd(prefix + "npc:allowedMoves", data.name.toLowerCase())
     .then(function(addReturned){
-      return QRedis.hmset("move:"+data.name.toLowerCase(),{
+      return QRedis.hmset(prefix + "move:"+data.name.toLowerCase(),{
         "power": data.power,
         "type": moves.getMoveType(data.name.toLowerCase())
       })
@@ -87,56 +93,56 @@ module.exports.addMoveNPC = function(data) {
 module.exports.setUserPkmnTypes = function(typesArray) {
   //TODO: use apply (or Q's version of it)
   if(typesArray[1]) {
-    return QRedis.sadd("user:pkmnTypes", typesArray[0], typesArray[1]);
+    return QRedis.sadd(prefix + "user:pkmnTypes", typesArray[0], typesArray[1]);
   } else {
-    return QRedis.sadd("user:pkmnTypes", typesArray[0]);
+    return QRedis.sadd(prefix + "user:pkmnTypes", typesArray[0]);
   }
 }
 
 module.exports.setNpcPkmnTypes = function(typesArray) {
   //TODO: use apply (or Q's version of it)
   if(typesArray[1]) {
-    return QRedis.sadd("npc:pkmnTypes", typesArray[0], typesArray[1]);
+    return QRedis.sadd(prefix + "npc:pkmnTypes", typesArray[0], typesArray[1]);
   } else {
-    return QRedis.sadd("npc:pkmnTypes", typesArray[0]);
+    return QRedis.sadd(prefix + "npc:pkmnTypes", typesArray[0]);
   }
 }
 
 module.exports.getUserPkmnTypes = function() {
-  return QRedis.smembers("user:pkmnTypes");
+  return QRedis.smembers(prefix + "user:pkmnTypes");
 }
 
 module.exports.getNpcPkmnTypes = function() {
-  return QRedis.smembers("npc:pkmnTypes");
+  return QRedis.smembers(prefix + "npc:pkmnTypes");
 }
 
 module.exports.getUserAllowedMoves = function() {
-  return QRedis.smembers("user:allowedMoves");
+  return QRedis.smembers(prefix + "user:allowedMoves");
 }
 module.exports.getNpcAllowedMoves = function() {
-  return QRedis.smembers("npc:allowedMoves");
+  return QRedis.smembers(prefix + "npc:allowedMoves");
 }
 
 module.exports.getSingleMove = function(moveName) {
-  return QRedis.hgetall("move:"+moveName.toLowerCase());
+  return QRedis.hgetall(prefix + "move:"+moveName.toLowerCase());
 }
 
 module.exports.setNpcHP = function(hp) {
-  return QRedis.set("npc:hp", hp);
+  return QRedis.set(prefix + "npc:hp", hp);
 }
 module.exports.getNpcHP = function() {
-  return QRedis.get("npc:hp");
+  return QRedis.get(prefix + "npc:hp");
 }
 
 module.exports.setUserHP = function(hp) {
-  return QRedis.set("user:hp", hp);
+  return QRedis.set(prefix + "user:hp", hp);
 }
 module.exports.getUserHP = function() {
-  return QRedis.get("user:hp");
+  return QRedis.get(prefix + "user:hp");
 }
 module.exports.doDamageToUser = function(damage) {
-  return QRedis.decrby("user:hp", damage);
+  return QRedis.decrby(prefix + "user:hp", damage);
 }
 module.exports.doDamageToNpc = function(damage) {
-  return QRedis.decrby("npc:hp", damage);
+  return QRedis.decrby(prefix + "npc:hp", damage);
 }
